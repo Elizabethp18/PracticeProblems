265849b406ec66c3fc08f7cb568f482ec14f97e0
Elizabeth Perkins <perkins.elizabeth.r@gmail.com>
2014-01-26 11:23:39 -0500

    Adds Anagrams class and tests

---
+++ src/main/java/Anagrams.java
@@ -0,0 +1,42 @@
+package main.java;
+
+public class Anagrams {
+	
+	public static Boolean checkForAnagram(String firstString, String secondString)  {
+		Boolean anagram = true;
+		
+		//assumes anagrams are not case sensitive
+		firstString = firstString.toLowerCase();
+		secondString = secondString.toLowerCase();
+		
+		if(firstString.length() != secondString.length())  {
+			anagram = false;
+		}
+		else  {
+			//assumes ASCII chars
+			int[] charsFound = new int[256];
+			char[] firstCharArray = firstString.toCharArray();
+			char[] secondCharArray = secondString.toCharArray();
+
+			for(int i = 0; i < firstCharArray.length; i++)  {
+				int charInt = firstCharArray[i];
+				System.out.println(charInt);
+				charsFound[charInt]++;
+			}
+			
+			for(int j = 0; j < secondCharArray.length; j++)  {
+				int secondStringCharInt = secondCharArray[j];
+				
+				if(charsFound[secondStringCharInt] == 0)  {
+					anagram = false;
+					break;
+				}
+				else  {
+					charsFound[secondStringCharInt]--;
+				}
+			}
+		}
+		
+		return anagram;
+	}
+}
---
+++ src/main/test/TestAnagrams.java
@@ -0,0 +1,52 @@
+package main.test;
+
+import junit.framework.Assert;
+
+import main.java.Anagrams;
+
+import org.junit.Test;
+
+public class TestAnagrams {
+
+	@Test
+	public void testForAnagramTrue() {
+		Boolean actual = Anagrams.checkForAnagram("abcdbcd", "bcdbcda");
+		
+		Assert.assertTrue(actual);
+	}
+	
+	@Test
+	public void testForAnagramTrueDifferingCases()  {
+		Boolean actual = Anagrams.checkForAnagram("AbcDbcd", "BCdbcda");
+		
+		Assert.assertTrue(actual);
+	}
+
+	@Test
+	public void testForAnagramTrueWithNums()  {
+		Boolean actual = Anagrams.checkForAnagram("abc123", "bca321");
+		
+		Assert.assertTrue(actual);
+	}
+	
+	@Test
+	public void testForAnagramTrueWithPunctuation()  {
+		Boolean actual = Anagrams.checkForAnagram("ab?!", "?!ba");
+		
+		Assert.assertTrue(actual);
+	}
+	
+	@Test
+	public void testForAnagramFalseUnequalLengths()  {
+		Boolean actual = Anagrams.checkForAnagram("abcd", "abc");
+		
+		Assert.assertFalse(actual);		
+	}
+	
+	@Test
+	public void testForAnagramsFalse()  {
+		Boolean actual = Anagrams.checkForAnagram("abcc", "abcd");
+		
+		Assert.assertFalse(actual);				
+	}
+}
